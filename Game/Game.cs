c7d302ec0dbb4db4d45ca3197f17a8edc48b4e74
Tony CC <tonycamezcha@gmail.com>
2020-08-13 23:38:05 -0500

    Added comments on Game,cs

--- Game/Game.cs
+++ Game/Game.cs
@@ -1,16 +1,19 @@
 using System;
 using System.Dynamic;
+using System.Threading;
 
 namespace ConsoleGame
 {
   class Game : SuperGame
   {
+    //method used to update the player's position dependant on the key pressed.
     public static new void UpdatePosition(string keyPressed, out int X, out int Y)
         {
 
             X = 0;
             Y = 0;
 
+            //changes the coordinates of the player based on the key pressed.
             switch (keyPressed) {
                 case "LeftArrow":
                     X -= 1;
@@ -29,11 +32,13 @@ namespace ConsoleGame
             }
         }
 
+    //updates the way the player is facing. 
     public static new char UpdateCursor(string keyInput)
         {
 
             char theChar = 'c';
 
+            //cardinal points. 
             switch (keyInput){
                 case "LeftArrow":
                     theChar = '<';
@@ -51,19 +56,24 @@ namespace ConsoleGame
                     break;
             }
 
+            //returns the char that shows the way the player moved.
             return theChar;
         }
+
+        //keeps the player inside the assigned space for the game.
         public static new int KeepInBounds(int coord, int maxValue)
         {
-
+            //if the x or y values exceeds the boundaries value, it sets the position to the origin.
             if (coord > maxValue)
             {
                 return 0;
             }
+            //if the x or y values are less than zero, it sets the position to the boundary limit.
             else if (coord < 0)
             {
                 return maxValue;
             }
+            //the player isn't affected whatsoever.
             else
             {
                 return coord;
@@ -71,12 +81,16 @@ namespace ConsoleGame
             
 
         }
+
+        //sets the number of '@' collected by the player.
         public static new bool DidScore(int charX, int charY, int fruitX, int fruitY)
         {
+            //if the position of the character and the fruit are the same for (x, y), returns true.
             if (charX == fruitX && charY == fruitY)
             {
                 return true;
             }   
+            //returns false if the conditions are not met.
             else
             {
                 return false;
